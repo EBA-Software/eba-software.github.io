6b213396558a1a7f34b481b36ca1d49a86063b50
EBA <official-eba@outlook.com>
2021-10-02 11:29:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,5 +16,5 @@ function dwnld(fileDir, name) {
 (function(c,l,a,r,i,t,y){
   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-  y=l.getElementsByTageName(r)[0];y.parentNode.insertBefore(t,y);
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "69wj77p43q");
