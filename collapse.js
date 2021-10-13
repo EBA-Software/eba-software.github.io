f24065a30502ce43e411f6f4326cf079766aad96
EBA <official-eba@outlook.com>
2021-10-13 11:52:28 -0400

    Update collapse.js

--- collapse.js
+++ collapse.js
@@ -5,7 +5,7 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click",function() {
     this.classList.toggle("active");
     var cont = this.nextElementSibling;
-    if (cont.syle.maxHeight){
+    if (cont.style.maxHeight){
       cont.style.maxHeight = null;
     } else {
       cont.style.maxHeight = cont.scrollHeight + "px";
