059b136661dd9cce175537b615db8e052686afb6
EBA <official-eba@outlook.com>
2021-10-25 14:52:33 -0400

    Update getOS.js

--- getOS.js
+++ getOS.js
@@ -2,51 +2,51 @@ var os = navigator.appVersion;
 var comp = "Unrecognized operating system: " + os;
 var isComp = 1
 if (os.indexOf("X11") != -1) {
-  comp = "EBA Command Center is not supported on Unix!";
+  comp = "This program is not supported on Unix!";
   isComp = 0;
 }
 if (os.indexOf("Windows NT 10") != -1) {
-  comp = "EBA Command Center is supported in Windows 10";
+  comp = "This program is supported in Windows 10";
   isComp = 2;
 }
 if (os.indexOf("Windows NT 8") != -1) {
-  comp = "EBA Command Center is supported in Windows 8";
+  comp = "This program is supported in Windows 8";
   isComp = 2;
 }
 if (os.indexOf("Windows NT 6.1") != -1) {
-  comp = "You're using Windows 7. You must use the Windows 7 edition of EBA Command Center";
+  comp = "You're using Windows 7. You must use the Windows 7 edition of this program";
   isComp = 2;
 }
 if (os.indexOf("Windows NT Vista") != -1) {
-  comp = "You're using Windows Vista. You must use the Windows 7 edition of EBA Command Center";
+  comp = "You're using Windows Vista. You must use the Windows 7 edition of this program";
   isComp = 2;
 }
 if (os.indexOf("Windows NT XP") != -1) {
-  comp = "You're using Windows XP. You must use the Windows XP edition of EBA Command Center";
+  comp = "You're using Windows XP. You must use the Windows XP edition of this program";
   isComp = 2;
 }
 if (os.indexOf("Mac") != -1) {
-  comp = "EBA Command Center is not supported on Mac!";
+  comp = "This program is not supported on Mac!";
   isComp = 0;
 }
 if (os.indexOf("iPhone") != -1) {
-  comp = "EBA Command Center is not supported on iPhone!";
+  comp = "This program is not supported on iPhone!";
   isComp = 0;
 }
 if (os.indexOf("Linux") != -1) {
-  comp = "EBA Command Center is not supported on Unix!";
+  comp = "This program is not supported on Unix!";
   isComp = 0;
 }
 if (os.indexOf("CrOS") != -1) {
-  comp = "EBA Command Center is not supported on ChromeOS!";
+  comp = "This program is not supported on ChromeOS!";
   isComp = 0;
 }
 if (os.indexOf("Android") != -1) {
-  comp = "EBA Command Center is not supported on Android!";
+  comp = "This program is not supported on Android!";
   isComp = 0;
 }
 if (os.indexOf("SMART-TV") != -1) {
-  comp = "EBA Command Center is not supported on SmartTV!";
+  comp = "This program is not supported on SmartTV!";
   isComp = 0;
 }
 
