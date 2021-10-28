11707411310a91353c6f4c9de31c917847e3155b
EBA <official-eba@outlook.com>
2021-10-28 09:50:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,41 @@ function dwnld(fileDir, name) {
   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "69wj77p43q");
 
+//Cookies
+function createCookie(name,value) {
+  document.cookie = name + "=" + value + "; path=/";
+}
+function getCookie(name) {
+  name = name + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
 //Dark Mode
 function toggleDark() {
   const themeStylesheet = document.getElementById('dark');
