a25c173593c6bb3fed3d5631506fa6afa358b014
EBA <official-eba@outlook.com>
2021-11-03 11:51:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ $(function(){
   $("#header").load("https://eba-software.github.io/header.html");
   $("#footer").load("https://eba-software.github.io/footer.html");
   $("#status").load("https://eba-software.github.io/status.html");
+  $("#status-dark").load("https://eba-software.github.io/status-dark.html");
 });
 
 //Create Functions
